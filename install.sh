16ae61412005a43100dc713a36dba8302c498764
Leonardional <leonardional@gmail.com>
2022-09-05 10:05:24 +0700

    remove endpoint

--- install.sh
+++ install.sh
@@ -139,8 +139,8 @@ install_fleet(){
     wget -O assets/elastic-agent-${ELASTICSEARCH_VERSION}-linux-x86_64.tar.gz https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-${ELASTICSEARCH_VERSION}-linux-x86_64.tar.gz
     tar xzvf assets/elastic-agent-${ELASTICSEARCH_VERSION}-linux-x86_64.tar.gz -C assets
     yes | sudo ./assets/elastic-agent-${ELASTICSEARCH_VERSION}-linux-x86_64/elastic-agent install --fleet-server-es=https://localhost:9200 --fleet-server-service-token=$(curl -k -u "elastic:$(tail -1 password-elasticsearch.txt | cut -d " " -f 3)" -s -X POST http://localhost:5601/api/fleet/service-tokens --header 'kbn-xsrf: true' | jq -r .value) --fleet-server-policy=ca-security-endpoint --fleet-server-es-ca-trusted-fingerprint=$(sudo openssl x509 -fingerprint -sha256 -noout -in /etc/elasticsearch/certs/http_ca.crt | awk -F"=" {' print $2 '} | sed s/://g)
-    sudo cat conf/endpoint >> /etc/kibana/kibana.yml
-    sudo systemctl restart kibana.service
+    # sudo cat conf/endpoint >> /etc/kibana/kibana.yml
+    # sudo systemctl restart kibana.service
     echo "[Step 9] Install Fleet Server Complete"
     echo ""
 	echo ""
